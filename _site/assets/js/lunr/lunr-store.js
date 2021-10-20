8147b9d855e268f7613e9cf2ca9bce9d4be2be21
ghchae <cvxgihyun@gmail.com>
2021-10-20 22:56:11 +0900

    bundle

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "첫 포스팅",
+        "excerpt":"공부한거 여기에 기록  마크다운 언어 사용  ","categories": [],
+        "tags": [],
+        "url": "/first/",
+        "teaser": null
+      }]
